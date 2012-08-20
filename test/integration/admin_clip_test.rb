3bf8f4020422f6b7775bd04736f19c002b49f6af
Jason Ronallo <jronallo@gmail.com>
2012-08-20 17:32:13 -0400

    update tests for the admin clips form that actually works

--- test/integration/admin_clip_test.rb
+++ test/integration/admin_clip_test.rb
@@ -18,8 +18,8 @@ class AdminClipTest < ActionDispatch::IntegrationTest
   
   test "adding duration to the form when filename entered" do
     visit(new_admin_kinney_clip_path)
-    assert page.find_field('clip_duration')
-    assert page.find_field('clip_duration').value.empty?
+    assert page.find_field('kinney_clip_duration')
+    assert page.find_field('kinney_clip_duration').value.empty?
     fill_in 'Filename', :with => 'tolson-roots'
     # This was having random failures without the wait_until and appears to even after adding it
     # the wait_until may be slowing down tests too much
@@ -30,14 +30,14 @@ class AdminClipTest < ActionDispatch::IntegrationTest
   
   test "add a description for a clip" do
     visit(new_admin_kinney_clip_path)
-    assert page.find_field('clip_description')
+    assert page.find_field('kinney_clip_description')
     fill_in 'Description', :with => 'This is a description of a new clip.'
   end
   
   test "display whether there is a VTT file for a clip" do
     visit(new_admin_kinney_clip_path)
     fill_in 'Filename', :with => 'atkins-campuslife_fire'
-    page.execute_script("$('#clip_filename').trigger('focus')") # to make sure focus is triggered  
+    page.execute_script("$('#kinney_clip_filename').trigger('focus')") # to make sure focus is triggered  
     assert page.has_content?('WEBVTT Found')
     fill_in 'Filename', :with => 'some-garbage'
     assert page.has_content?('WEBVTT Not Found')
@@ -45,12 +45,12 @@ class AdminClipTest < ActionDispatch::IntegrationTest
 
   test "do not allow submission of clip if there is no webvtt file" do
     visit(new_admin_kinney_clip_path)
-    assert page.has_selector?("#clip_submit[@disabled='disabled']")
+    assert page.has_selector?("#kinney_clip_submit[@disabled='disabled']")
     fill_in 'Filename', :with => 'atkins-campuslife_fire'
-    page.execute_script("$('#clip_filename').trigger('focus')") # to make sure focus is triggered
-    assert page.has_no_selector?("#clip_submit[@disabled='disabled']")
+    page.execute_script("$('#kinney_clip_filename').trigger('focus')") # to make sure focus is triggered
+    assert page.has_no_selector?("#kinney_clip_submit[@disabled='disabled']")
     fill_in 'Filename', :with => 'atkins-campuslife'
-    assert page.has_selector?("#clip_submit[@disabled='disabled']")
+    assert page.has_selector?("#kinney_clip_submit[@disabled='disabled']")
   end
   
 end
