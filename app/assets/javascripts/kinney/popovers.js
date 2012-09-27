cd36f8324123d3a68892eff6319672cdfaf73c2d
Jason Ronallo <jronallo@gmail.com>
2012-09-27 15:52:51 -0400

    move popovers and modals into popovers.js. fix modals so that remote target is not loaded

--- app/assets/javascripts/kinney/popovers.js
+++ app/assets/javascripts/kinney/popovers.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
-  // $('.video_topics').popover();
-  // $("[rel='popover']").popover();
-  // $("[rel='tooltip']").tooltip();
+  //Popovers
+  Kinney.twitter_bootstrap_popover_placement = function(){
+    var width = window.innerWidth;
+    if ($('#kinney_people_show').length > 0){
+      return 'bottom';
+    }
+    if (width<900) return 'bottom';
+    // FIXME: is there a better way to get the parent of the element which triggered the popover?
+    if ($($(this)[0].$element[0]).parent().hasClass('odd')) {
+      return 'right';
+    }
+    return 'left';
+  }
 
-  $('.image_modal').click(function(e){
+  // This is a test that ought to work in testing for touch devices
+  Kinney.is_touch_device = function() {
+    return !!('ontouchstart' in window);
+  }
+
+  // We only want popovers to show up on the desktop. These are currently just
+  // informational, so we do not need to show them on touch devices.
+  if ( !Kinney.is_touch_device() ){
+    $('[rel="popover"]').popover({
+      trigger: 'hover',
+      placement: Kinney.twitter_bootstrap_popover_placement
+    });
+  } 
+
+
+  // Modals
+  $('.image_modal_trigger').click(function(e){
     var data_target = $(this).data('target');
     $(data_target).modal({
       remote: false
