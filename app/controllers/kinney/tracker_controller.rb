b16da2bda91d9d3938bad2b2573089879beeebbc
Jason Ronallo <jronallo@gmail.com>
2013-04-01 17:20:30 -0400

    analysis is no longer under the tracker controller so we can remove this

--- app/controllers/kinney/tracker_controller.rb
+++ app/controllers/kinney/tracker_controller.rb
@@ -1,6 +1,5 @@
 module Kinney
   class TrackerController < KinneyController
-    before_filter :authenticate_admin_user!, :only => :analyze
 
     def track
       if request.xhr?
@@ -14,38 +13,5 @@ module Kinney
       end
     end
 
-    def analyze
-      @clip = Clip.where(:filename => params[:video]).first
-      # This is all a mess and could definitely be cleaned up!
-      @uuids = Tracker.where(:video => params[:video]).pluck(:uuid).uniq    
-      @all_seconds = []
-      @uuids.each do |uuid|
-        @all_seconds << Tracker.where(:video => params[:video], :uuid => uuid).pluck(:seconds).flatten.uniq      
-      end   
-      @all_seconds.flatten! 
-      @all_seconds = @all_seconds.map{|second| second.to_i}
-      
-      @highest_value = 0
-
-      # @seconds should be a hash like this {'second' => 1, 'value' => 2}
-      # We start off with a hash which is the length of the duration of the clip, and
-      # then for every unique occurrence of that second in a tracker we increment the
-      # value by 1.
-      @temp_seconds = Hash[(0..@clip.duration).map{|num| [num, 0]}]
-
-      @all_seconds.each do |n| 
-        @temp_seconds[n] += 1
-      end
-      @seconds = @temp_seconds.map do |k,v|
-        if v > @highest_value
-          @highest_value = v
-        end
-        {'second' => k,
-          'value' => v}
-      end.sort_by{|second| second["second"]}
-
-
-      
-    end
   end
 end
