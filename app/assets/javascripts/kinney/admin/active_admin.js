2a256d3945f6063f0fcd866dd1c6d67819aa843e
Jason Ronallo <jronallo@gmail.com>
2013-09-27 10:55:53 -0400

    do not include mediaelement player in admin interface

--- app/assets/javascripts/kinney/admin/active_admin.js
+++ app/assets/javascripts/kinney/admin/active_admin.js
@@ -1,9 +1,6 @@
 //= require active_admin/base
 //= require kinney/admin/images
 //= require kinney/admin/video_preview
-//#= require mediaelement_rails/mediaelement
-//#= require mediaelement_rails/mediaelementplayer
-//#= require mediaelement_rails/rails
 //= require chosen-jquery
 
 $(document).ready(function() {
