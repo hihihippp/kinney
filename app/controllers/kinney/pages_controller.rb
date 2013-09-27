26aad47f1a2750ac6a7c55f741e3094b6a1cfaf4
Jason Ronallo <jronallo@gmail.com>
2013-09-26 20:07:33 -0400

    remove action caching from pages controller

--- app/controllers/kinney/pages_controller.rb
+++ app/controllers/kinney/pages_controller.rb
@@ -1,10 +1,8 @@
 module Kinney
   class PagesController < KinneyController
 
-    caches_action :about, :home
-
     def about
-      
+
     end
 
     def home
@@ -16,7 +14,7 @@ module Kinney
       if @featured_clip.blank?
         @featured_clip = @top_pick_clips.pop
       end
-      @person = @featured_clip.people.first if @featured_clip    
+      @person = @featured_clip.people.first if @featured_clip
       # explore topics
       @topics_with_clips = Topic.with_clips
     end
