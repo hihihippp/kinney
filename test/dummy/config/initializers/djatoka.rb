4fd5754f2f5180f8e04da1a76ea226bd29d55cef
Jason Ronallo <jronallo@gmail.com>
2013-10-17 17:05:38 -0400

    remove djatoka initializer

--- test/dummy/config/initializers/djatoka.rb
+++
@@ -1,7 +0,0 @@
-if Djatoka.respond_to? :resolver
-  if Rails.env=='development'
-    Djatoka.resolver="http://scrc.lib.ncsu.edu/adore-djatoka/resolver"#"http://localhost/adore-djatoka/resolver"
-  else
-    Djatoka.resolver="http://scrc.lib.ncsu.edu/adore-djatoka/resolver"
-  end
-end
